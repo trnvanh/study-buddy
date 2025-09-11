c1c6adc55adb428afec83eb89be7bdfbd12e4030
Tran Van Anh <anh.v.tran@tuni.fi>
2025-09-11 20:46:26 +0300

    updated backend for stats

--- backend/src/main/java/com/project/study/repository/PomodoroLogRepository.java
+++ backend/src/main/java/com/project/study/repository/PomodoroLogRepository.java
@@ -17,31 +17,12 @@ public interface PomodoroLogRepository extends JpaRepository<PomodoroLogEntity, 
     //@Query("SELECT p FROM PomodoroLog p WHERE p.pet.id = :petId AND p.startTime >= :start AND p.startTime <= :end")
     //List<PomodoroLogEntity> findByPet_IdAndStartTimeBetween(Long petId, LocalDateTime start, LocalDateTime end);
 
-    // Daily count
-    @Query("SELECT COUNT(p) FROM PomodoroLogEntity p WHERE p.endTime >= :startOfDay AND p.endTime < :endOfDay")
-    long countDaily(@Param("startOfDay") LocalDateTime startOfDay,
-                    @Param("endOfDay") LocalDateTime endOfDay);
-
-    // Weekly count
-    @Query("SELECT COUNT(p) FROM PomodoroLogEntity p WHERE p.endTime >= :startOfWeek AND p.endTime < :endOfWeek")
-    long countWeekly(@Param("startOfWeek") LocalDateTime startOfWeek,
-                     @Param("endOfWeek") LocalDateTime endOfWeek);
-
-    // Monthly count
-    @Query("SELECT COUNT(p) FROM PomodoroLogEntity p WHERE p.endTime >= :startOfMonth AND p.endTime < :endOfMonth")
-    long countMonthly(@Param("startOfMonth") LocalDateTime startOfMonth,
-                      @Param("endOfMonth") LocalDateTime endOfMonth);
-
-    // Yearly count
-    @Query("SELECT COUNT(p) FROM PomodoroLogEntity p WHERE p.endTime >= :startOfYear AND p.endTime < :endOfYear")
-    long countYearly(@Param("startOfYear") LocalDateTime startOfYear,
-                     @Param("endOfYear") LocalDateTime endOfYear);
-
-    @Query("SELECT COUNT(p) FROM PomodoroLogEntity p WHERE p.endTime >= :start AND p.endTime < :end")
+    // Count time in a period (daily, weekly, monthly, etc)
+    @Query("SELECT COUNT(p) FROM pomodoro_log_entity p WHERE p.end_time >= :start AND p.end_time < :end")
     long countBetween(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
     // Count total
-    @Query("SELECT SUM(p.durationMinutes) FROM PomodoroLogEntity p")
-    Long sumAllDurations();
+    @Query("SELECT SUM(p.duration_minutes) FROM pomodoro_log_entity p")
+    long sumAllDurations();
 
 }
